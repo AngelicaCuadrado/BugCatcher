ee111950a2db27aa4c9365091d7786b50c87eccd
sduffney <148494340+sduffney@users.noreply.github.com>
2025-12-02 16:56:23 -0500

    Ladybugs

--- Assets/Scripts/Swarms/Ladybug.cs
+++ Assets/Scripts/Swarms/Ladybug.cs
@@ -2,15 +2,77 @@ using UnityEngine;
 
 public class Ladybug : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    internal LadybugFlockController controller;
+    private new Rigidbody rigidbody;
+    private void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        controller = (LadybugFlockController)GameObject.FindGameObjectWithTag("LadybugFlockController").GetComponent("LadybugFlockController");
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        if (controller)
+        {
+            Vector3 relativePos = Steer() * Time.deltaTime;
+            if (relativePos != Vector3.zero)
+                rigidbody.linearVelocity = relativePos;
+            float speed = rigidbody.linearVelocity.magnitude;
+            if (speed > controller.maxVelocity)
+            {
+                rigidbody.linearVelocity = rigidbody.linearVelocity.normalized *
+                controller.maxVelocity;
+            }
+            else if (speed < controller.minVelocity)
+            {
+                rigidbody.linearVelocity = rigidbody.linearVelocity.normalized *
+                controller.minVelocity;
+            }
+
+            if (transform.position.y < 0f)
+            {
+                Vector3 pos = transform.position;
+                pos.y = 0f;
+                transform.position = pos;
+
+
+                if (rigidbody.linearVelocity.y < 0f)
+                    rigidbody.linearVelocity = new Vector3(rigidbody.linearVelocity.x, 0f, rigidbody.linearVelocity.z);
+            }
+
+            if (transform.position.y > 2f)
+            {
+                Vector3 pos = transform.position;
+                pos.y = 2f;
+                transform.position = pos;
+
+
+                if (rigidbody.linearVelocity.y > 0f)
+                    rigidbody.linearVelocity = new Vector3(rigidbody.linearVelocity.x, 0f, rigidbody.linearVelocity.z);
+            }
+        }
+    }
+    private Vector3 Steer()
+    {
+        Vector3 center = controller.flockCenter - transform.position;
+        Vector3 velocity = controller.flockVelocity - rigidbody.linearVelocity;
+        Vector3 follow = controller.target.position - transform.position;
+        Vector3 separation = Vector3.zero;
+        foreach (Ladybug ladybug in controller.flockList)
+        {
+            if (ladybug != this)
+            {
+                Vector3 relativePos = transform.position - ladybug.transform.position;
+                separation += relativePos.normalized;
+            }
+        }
+        Vector3 randomize = new Vector3(Random.value * 2 - 1, Random.value * 2 - 1, Random.value * 2 - 1);
+        randomize.Normalize();
+        return (
+        controller.centerWeight * center
+        + controller.velocityWeight * velocity
+        + controller.separationWeight * separation
+        + controller.followWeight * follow
+        + controller.randomizeWeight * randomize
+        );
     }
 }
--- Assets/Scripts/Swarms/LadybugFlockController.cs
+++ Assets/Scripts/Swarms/LadybugFlockController.cs
@@ -1,16 +1,114 @@
+using System.Collections;
 using UnityEngine;
 
 public class LadybugFlockController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public float minVelocity = 1;
+    public float maxVelocity = 8;
+    public int flockSize = 5;
+    public float centerWeight = 1;
+    public float velocityWeight = 1;
+    public float separationWeight = 1;
+    public float followWeight = 1;
+    public float randomizeWeight = 1;
+    public Ladybug ladybugPrefab;
+    public Transform target;
+    public Vector3 flockCenter;
+    internal Vector3 flockVelocity;
+    public ArrayList flockList = new ArrayList();
+
+    public SwarmFSM fsm;
+
+    private Transform evadeTarget;
+
+
+    public GameObject player;
+    public float agroRange = 2f;
     void Start()
     {
-        
+        evadeTarget = new GameObject("EvadeTarget").transform;
+        evadeTarget.parent = transform;
+        evadeTarget.position = transform.position;
+
+        fsm = new SwarmFSM();
+        flockSize = Random.Range(1, flockSize + 1);
+        player = FindFirstObjectByType<PlayerHealth>().gameObject;
+        target = transform;
+        for (int i = 0; i < flockSize; i++)
+        {
+            Ladybug ladybug = Instantiate(ladybugPrefab, transform.position, transform.rotation);
+            ladybug.transform.parent = transform;
+            ladybug.controller = this;
+            flockList.Add(ladybug);
+        }
+
+        //States
+        //Evade
+        var evadeState = fsm.CreateState("Evade");
+        evadeState.onEnter = delegate
+        {
+            Vector3 dir = transform.position - player.transform.position;
+            dir.y = 0f;
+            dir = dir.normalized;
+
+            evadeTarget.position = transform.position + dir * agroRange * 2f;
+
+            evadeTarget.position = new Vector3(evadeTarget.position.x, transform.position.y, evadeTarget.position.z);
+
+            target = evadeTarget;
+        };
+
+        evadeState.onStay = delegate
+        {
+            Vector3 dir = transform.position - player.transform.position;
+            dir.y = 0f;
+            dir = dir.normalized;
+
+            evadeTarget.position = transform.position + dir * agroRange * 2f;
+
+            evadeTarget.position = new Vector3(evadeTarget.position.x, transform.position.y, evadeTarget.position.z);
+
+            target = evadeTarget;
+        };
+
+        evadeState.onExit = delegate
+        {
+            target = transform;
+        };
+
+        //Idle
+        var idleState = fsm.CreateState("Idle");
+        idleState.onEnter = delegate
+        {
+        };
+
+        idleState.onStay = delegate
+        {
+            transform.position = Vector3.Lerp(transform.position, flockCenter, Time.deltaTime * 0.5f);
+        };
+
+        idleState.onExit = delegate
+        {
+
+        };
+
+        //Transitions
+        fsm.AddTransition("Idle", "Evade", () => Vector3.Distance(transform.position, player.transform.position) <= agroRange);
+        fsm.AddTransition("Evade", "Idle", () => Vector3.Distance(transform.position, player.transform.position) >= agroRange);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        fsm.Update();
+
+        Vector3 center = Vector3.zero;
+        Vector3 velocity = Vector3.zero;
+        foreach (Ladybug ladybug in flockList)
+        {
+            center += ladybug.transform.position;
+            velocity += ladybug.GetComponent<Rigidbody>().linearVelocity;
+        }
+        flockCenter = center / flockSize;
+        flockVelocity = velocity / flockSize;
     }
 }
