299aac3f1cdde391666088a845121676cf5b9152
sduffney <148494340+sduffney@users.noreply.github.com>
2025-12-09 20:05:59 -0500

    Started Level Tracker

--- Assets/Scripts/LevelComponents/Scene_Manager.cs
+++ Assets/Scripts/LevelComponents/Scene_Manager.cs
@@ -1,19 +1,16 @@
 using UnityEngine;
-using UnityEngine.SceneManagement; // Needed for scene loading
+using UnityEngine.SceneManagement;
 
 public class Scene_Manager : MonoBehaviour
 {
-    // Call this function from a UI Button
     public void PlayGame()
     {
-        // Replace "Level_1" with the exact name of your scene
         SceneManager.LoadScene("Level_1");
     }
 
-    // Optional: quit button
     public void QuitGame()
     {
         Application.Quit();
-        Debug.Log("Game closed!"); // Works only in build, not in editor
+        Debug.Log("Game closed!");
     }
 }
